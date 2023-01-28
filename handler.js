7b39580e0734e052f50c08e656f5674b963e537e
Jenktaki123 <110096965+Jenktaki123@users.noreply.github.com>
2023-01-28 11:06:22 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,11 +5,13 @@ const configuration = new Configuration({apiKey: apikey.openai});
 const openai = new OpenAIApi(configuration);
 
 module.exports = index = async (api,event) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: event.body,
-    temperature: 0.3,
-    max_tokens: 3000,
-  });
-  api.sendMessage(`${response.data.choices[0].text}`, event.threadID)
+  if (event.body.startsWith("Faith")) {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: event.body,
+      temperature: 0.3,
+      max_tokens: 3000,
+    });
+    api.sendMessage(`${response.data.choices[0].text}`, event.threadID)
+  }
 }
